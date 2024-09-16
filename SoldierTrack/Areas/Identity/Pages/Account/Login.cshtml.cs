7aea1ed5ca4799e26da9d414854f2f274b805cd1
milewSasho <mileww.sasho@abv.bg>
2024-09-16 22:19:50 +0300

    Remove unnecessary code from login and register pages

---
+++ SoldierTrack/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,95 @@
+﻿#nullable disable
+namespace SoldierTrack.Web.Areas.Identity.Pages.Account
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly ILogger<LoginModel> logger;
+
+        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
+        {
+            this.signInManager = signInManager;
+            this.logger = logger;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Display(Name = "Remember me?")]
+            public bool RememberMe { get; set; }
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            if (!string.IsNullOrEmpty(this.ErrorMessage))
+            {
+                this.ModelState.AddModelError(string.Empty, ErrorMessage);
+            }
+
+            returnUrl ??= this.Url.Content("~/");
+            await this.HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+            this.ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= this.Url.Content("~/");
+
+            if (this.ModelState.IsValid)
+            {
+                var result = await this.signInManager
+                    .PasswordSignInAsync(
+                        this.Input.Email, 
+                        this.Input.Password, 
+                        this.Input.RememberMe, 
+                        lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    this.logger.LogInformation("User logged in.");
+                    return this.LocalRedirect(returnUrl);
+                }
+
+                if (result.RequiresTwoFactor)
+                {
+                    return this.RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, this.Input.RememberMe });
+                }
+
+                if (result.IsLockedOut)
+                {
+                    this.logger.LogWarning("User account locked out.");
+                    return this.RedirectToPage("./Lockout");
+                }
+                else
+                {
+                    this.ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return this.Page();
+                }
+            }
+
+            return this.Page();
+        }
+    }
+}
---
+++ SoldierTrack/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,107 @@
+﻿#nullable disable
+namespace SoldierTrack.Web.Areas.Identity.Pages.Account
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly IUserStore<IdentityUser> userStore;
+        private readonly ILogger<RegisterModel> logger;
+
+        public RegisterModel(
+            UserManager<IdentityUser> userManager,
+            IUserStore<IdentityUser> userStore,
+            SignInManager<IdentityUser> signInManager,
+            ILogger<RegisterModel> logger)
+        {
+            this.userManager = userManager;
+            this.userStore = userStore;
+            this.signInManager = signInManager;
+            this.logger = logger;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(
+                100,
+                ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
+                MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+
+        public void OnGet(string returnUrl = null) => this.ReturnUrl = returnUrl;
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= this.Url.Content("~/");
+
+            if (this.ModelState.IsValid)
+            {
+                var user = this.CreateUser();
+                await this.userStore.SetUserNameAsync(user, this.Input.Email, CancellationToken.None);
+                var result = await this.userManager.CreateAsync(user, this.Input.Password);
+
+                if (result.Succeeded)
+                {
+                    this.logger.LogInformation("User created a new account with password.");
+
+                    if (this.userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return this.RedirectToPage("RegisterConfirmation", new { email = this.Input.Email, returnUrl });
+                    }
+                    else
+                    {
+                        await this.signInManager.SignInAsync(user, isPersistent: false);
+                        return this.LocalRedirect(returnUrl);
+                    }
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    this.ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return this.Page();
+        }
+
+        private IdentityUser CreateUser()
+        {
+            try
+            {
+                return Activator.CreateInstance<IdentityUser>();
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Can't create an instance of '{nameof(IdentityUser)}'. " +
+                    $"Ensure that '{nameof(IdentityUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
+            }
+        }
+    }
+}
