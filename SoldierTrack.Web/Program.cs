2832866f25839ae260d2f9245200bf600a4374cb
aleksandarMilev <aleksandarmilev23@gmail.com>
2025-08-11 16:33:00 +0300

    create admin role

--- SoldierTrack.Web/Program.cs
+++ SoldierTrack.Web/Program.cs
@@ -51,4 +51,49 @@ if (envIsDevelopment)
     await app.CreateAdminRoleAsync();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<Athlete>>();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var adminEmail = Environment.GetEnvironmentVariable("TEMP_ADMIN_EMAIL");
+    var adminPassword = Environment.GetEnvironmentVariable("TEMP_ADMIN_PASSWORD");
+    const string adminRole = "Administrator";
+
+    if (string.IsNullOrWhiteSpace(adminEmail) || string.IsNullOrWhiteSpace(adminPassword))
+    {
+        throw new Exception("TEMP_ADMIN_EMAIL or TEMP_ADMIN_PASSWORD not set in environment variables.");
+    }
+
+    if (!await roleManager.RoleExistsAsync(adminRole))
+    {
+        await roleManager.CreateAsync(new IdentityRole(adminRole));
+    }
+
+    var adminUser = await userManager.FindByEmailAsync(adminEmail);
+    if (adminUser == null)
+    {
+        adminUser = new Athlete
+        {
+            FirstName = adminRole,
+            LastName = adminRole,
+            UserName = adminEmail,
+            Email = adminEmail,
+            EmailConfirmed = true
+        };
+
+        var result = await userManager.CreateAsync(adminUser, adminPassword);
+        if (!result.Succeeded)
+        {
+            throw new Exception("Failed to create admin: " + string.Join(", ", result.Errors.Select(e => e.Description)));
+        }
+    }
+
+    if (!await userManager.IsInRoleAsync(adminUser, adminRole))
+    {
+        await userManager.AddToRoleAsync(adminUser, adminRole);
+    }
+}
+
+
 await app.RunAsync();
